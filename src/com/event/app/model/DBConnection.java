a51c92cf732c302907f9c0a73b10715398ca8b3a
N00134452 <N00134452@A012-24.institute.local>
2015-02-17 17:02:08 +0000

    Updated comments

--- src/com/event/app/model/DBConnection.java
+++ src/com/event/app/model/DBConnection.java
@@ -7,14 +7,15 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static Connection sConnection;
-
+    
+    // Implement the DBConnection class as a singleton
     public static Connection getInstance() throws ClassNotFoundException, SQLException {
         String host, db, user, password;
         
         host = "daneel";
-        db = "john";
-        user = "john";
-        password = "secret";
+        db = "n00134452";
+        user = "N00134452";
+        password = "N00134452";
         
         if (sConnection == null || sConnection.isClosed()) {
             String url = "jdbc:mysql://" + host + "/" + db;
